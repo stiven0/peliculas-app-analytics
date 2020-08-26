ee32b805bb1aee8e944916314bfad262b4cc896d
stiven <stiven2750@gmail.com>
2020-08-26 11:49:43 -0500

    Google analytics aggregate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+declare var gtag: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'peliculas';
+
+  constructor(
+    private router: Router
+  ) {
+    const navEndEvents$ = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-169826429-1', {
+        // tslint:disable-next-line: object-literal-key-quotes
+        'page_path': event.urlAfterRedirects
+      });
+    });
+
+  }
+
 }
